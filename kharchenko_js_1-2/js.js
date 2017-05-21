bf0c971c69862c54d2baaa769ec5ff8f1f17adb9
SNKharchenko <svetaxa@gmail.com>
2017-05-21 22:59:03 +0300

    1-2

---
+++ kharchenko_js_1-2/js.js
@@ -0,0 +1,41 @@
+﻿//1
+
+function pow(x, n){
+	var result = 1; 
+	
+	for (var i = 0; i < n; i++){
+		
+	result *= x;} 
+		
+	return result;
+}
+
+var x = prompt("Enter a number", '');
+
+var n = prompt("Enter a power of number", '');
+
+var resultPow = pow(x, n);
+
+console.log(resultPow);
+
+//2
+
+var arr=[];
+ for (var i = 0; i < 5; i++){
+	 arr[i] = prompt('Enter the name');
+ }
+ 
+ var userName = prompt('what is your name?');
+ 
+ for (var i = 0; i < 5; i++){
+	 if (arr[i] == userName){
+		 alert(userName + ' you have entered successfully!');
+	 }else{
+		 alert('error');
+	 }
+ }
+
+
+
+
+
