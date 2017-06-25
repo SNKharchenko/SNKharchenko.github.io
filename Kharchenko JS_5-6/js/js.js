9e1f87b3c894efe2175da3e66be8974ff4d2f450
SNKharchenko <svetaxa@gmail.com>
2017-06-25 22:18:32 +0300

    5-6

--- Kharchenko JS_5-6/js/js.js
+++ Kharchenko JS_5-6/js/js.js
@@ -1,28 +1,31 @@
 (function() {
   //'use strict';
-    
+        
     var root = document.getElementById('root');
     
     var timeTable = document.createElement('div');
         timeTable.id = 'block';
         timeTable.classList.add('text-center', 'badge'); 
-        timeTable.style.marginLeft = '430px';
+        timeTable.style.marginLeft = '450px';
+        timeTable.style.display = 'inline-block';
         timeTable.style.marginBottom = '30px';
     
     var btnTable = document.createElement('div');
         btnTable.id = 'btn';
-        btnTable.classList.add('col-md-2', 'col-md-offset-5', 'text-center'); 
+        btnTable.style.marginLeft = '400px'; 
     
     var startBtn = document.createElement('button');
         startBtn.textContent = 'Start';
         startBtn.classList.add('btn', 'btn-success', 'btn-lg', 'active');
         startBtn.setAttribute('type', 'button');
         startBtn.style.marginRight = '30px';
+        startBtn.addEventListener('click', startTimer);
     
     var clearBtn = document.createElement('button');
         clearBtn.textContent = 'Clear';
         clearBtn.classList.add('btn', 'btn-danger', 'btn-lg', 'active');
         clearBtn.setAttribute('type', 'button');
+        clearBtn.addEventListener('click', clearTimer);
     
     var timerText = document.createElement('p');
         timerText.textContent = '00:00:00';
@@ -33,108 +36,88 @@
     root.append(timeTable, btnTable);
     timeTable.append(timerText, timerMilSec);
     btnTable.append(startBtn, clearBtn);
-    
-    var startTime = null,
-        timerID = null,
-        oddsTime = null,
-        ourTime = null,
-        active = false;
-    
-    
-     
-    startBtn.addEventListener('click', function () {
-        if (!active) startTime = Date.now();
-                    
-        timerID = setInterval(function getTime() {
+
+var startTime = 0,
+    oddsTime = 0,
+    ourTime = 0,
+    timeHours = 0,
+    timeSec = 0,
+    timerID = 0;
+
+
+function countTime() {
+	startTime = Date.now();        
+        timerID = setInterval(function() {
             oddsTime = Date.now() - startTime;
             ourTime = new Date(oddsTime);
             ourTime.setHours(0);
             
-            var hours = ourTime.getHours(),
+                var hours = ourTime.getHours(),
                 minutes = ourTime.getMinutes(),
-                seconds = ourTime.getSeconds();
+                seconds = ourTime.getSeconds(),
+                millisec = ourTime.getMilliseconds(); 
             
             if (hours < 10) {
-			var hoursNum = '0' + hours;    
+			hoursNum = '0' + hours;    
 		} else {
 			hoursNum = hours;
     }
             if (minutes < 10) {
-			var minutesNum = '0' + minutes;
+			 minutesNum = '0' + minutes;
 		} else {
 			minutesNum = minutes;
     }
             
             if (seconds < 10) {
-			var secondsNum = '0' + seconds;
+			 secondsNum = '0' + seconds;
 		} else {
 			secondsNum = seconds;
-    }
-
-            timerMilSec.textContent = ourTime.getMilliseconds();
-            timerText.textContent= hoursNum + ':' + minutesNum + ':' + secondsNum;
+    }    
+            
+            timeHours = hoursNum + ':' + minutesNum + ':' + secondsNum;
+            timerText.textContent = timeHours; 
+            
+            timeSec = millisec;
+            timerMilSec.textContent = timeSec;
+            
             
         }, 1); 
-        
     
-     if (startBtn === 'Start') {
-		getTime();
-		startBtn.textContent = 'Pause';
-        startBtn.classList.remove('btn', 'btn-success', 'btn-lg', 'active');
-         startBtn.classList.add('btn', 'btn-info', 'btn-lg', 'active');
-         
-    } else if (startBtn === 'Pause') {
-		stopTimer();
-		startBtn.textContent = 'Cont..';
-    }else if (startBtn === 'Cont..'){
-        getTime();
-        startBtn.textContent = 'Pause'; 
-        startBtn.classList.remove('btn', 'btn-success', 'btn-lg', 'active');
-         startBtn.classList.add('btn', 'btn-info', 'btn-lg', 'active');
-        }
-    });
-    
-    function stopTimer() {
-          clearInterval(timerID);
-          active = false;
-        }
-    	
-    
- /*   function startTimer(){
+}  
+ 	 
+function startTimer(){
+	startBtn.classList.add('btn-info');
+	startBtn.classList.remove('btn-success');
 	startBtn.textContent = 'Pause';
-	var timer = setInterval(timerID, 1);
+	countTime();
+	startBtn.removeEventListener("click", startTimer);
+	startBtn.addEventListener("click", pauseTimer);
 }
-    startBut.removeEventListener('click', startTimer);
-	startBut.addEventListener('click', pauseTimer);
-    
 
 function pauseTimer(){
-	startBtn.textContent = 'Continue';
-	clearInterval(timer);
-    timeTable.textContent = time;
+	startBtn.classList.add('btn-success');
+	startBtn.classList.remove('btn-info');
+	startBtn.textContent = 'Cont..';
+	clearInterval(timerID);
+    timerText.textContent = timeHours;
+    timerMilSec.textContent = timeSec;
 	startBtn.removeEventListener('click', pauseTimer);
 	startBtn.addEventListener('click', startTimer);
-}*/
- 
+}
 
-    clearBtn.addEventListener('click', function () {
-        clearInterval(timerID);
-        timerText.textContent = "00:00:00";
-	    startBtn.textContent = "start";
-        timerMilSec.textContent = '0';
-        active = false;
-        oddsTime = 0;
-}) ; 
-    
-      
-   /* function resetTimer() {
-	  active = false;
-	  clearInterval(timerID);
-	  timerText.textContent = "00:00:00";
-      startBtn.textContent = "start";
-      timerMilSec.textContent = '0';
-	}
-    
-    clearBtn.addEventListener('click', resetTimer);*/
+function clearTimer(){
+	startBtn.textContent = 'Start';
+	timerText.textContent = '00:00:00';
+    timerMilSec.textContent = '0';
+	clearInterval(timerID);
+	startBtn.removeEventListener('click', pauseTimer);
+	startBtn.addEventListener('click', startTimer);
+	startTime = 0;
+    oddsTime = 0;
+    ourTime = 0;
+    timeHours = 0;
+    timeSec = 0;
+    timerID = 0;
+}
     
 }) ();
